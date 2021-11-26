76c95686bc6d2c4f37d23fc584bae3a047d4d30a
Nicolò Coltro <coltro.school@gmail.com>
2021-11-26 16:38:59 +0100

    ripasso1

---
+++ Ripasso_Socket/Socket1_Client/Socket1_Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Socket1_Client {
+    
+    static class Client {
+        static public void Action() {
+            
+            // Connessione al Server
+            TcpClient c_client = new TcpClient("127.0.0.1", 2003); // Mi connetto al server con Ip = QuestoPc & porta = 2003 (scelta)
+            NetworkStream c_stream = c_client.GetStream(); // Il "Cliente" inizializza un canale di comunicazione bidirezionale con il "Delegato" del server
+
+            Byte[] c_buffer; // "Scatola" per l'informazione che viene inviata (quindi inviata+ricevuta) sullo stream preposto
+
+            // Fase di scambio informazioni
+            c_buffer = System.Text.Encoding.ASCII.GetBytes("Messaggio di test 123!"); // Creo una "Scatola" da inviare poi sullo stream
+            c_stream.Write(c_buffer, 0, c_buffer.Length); // Invio la "Scatola", che contiene il mio messaggio, sul canale di comunicazione
+            
+            // Saluti finali
+            c_stream.Close(); // Interrompi "canale di comunicazione"
+            c_client.Close(); // Saluta ed abbandona il "Delegato del Server"
+        }
+    }
+    
+    class Program {
+        static void Main(string[] args) {
+            Client.Action();
+        }
+    }
+}
---
+++ Ripasso_Socket/Socket1_Server/Socket1_Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Socket1_Server {
+
+    static class Server {
+        static public void Action() {
+            
+            // Fase di setup
+            IPAddress s_ip = Dns.GetHostAddresses("127.0.0.1")[0]; // Prendo il primo tra gli IP del PC (ricorda: 127.0.0.1 = lookback = questoPc!)
+            TcpListener s_listener = new TcpListener(s_ip, 2003); // Nuovo "Portinaio" => questo Pc, porta a scelta (2003 mi piace.)
+            s_listener.Start(); // Starto il "Portinaio"
+
+            // Fase di attesa e "Accoglienza Cliente"
+            TcpClient s_client = s_listener.AcceptTcpClient(); // Appena arriva un "Cliente", il "Portinaio" passa la richiesta ad un nuovo "Delegato" del server
+            NetworkStream s_stream = s_client.GetStream(); // Il "Delegato" inizializza un canale di comunicazione bidirezionale con il Cliente
+
+            Byte[] s_buffer = new Byte[1024]; // "Scatola" per l'informazione che viene ricevuta (quindi inviata+ricevuta) sullo stream preposto
+            
+            // Fase di scambio informazioni
+            while (s_stream.Read(s_buffer, 0, s_buffer.Length) > 0) { // Finché ci sono "Scatole" sullo stream
+                Console.WriteLine(System.Text.Encoding.ASCII.GetString(s_buffer)); // Leggi contenuto scatola, traducilo e stampalo a video
+                s_buffer = new Byte[1024];
+            }
+
+            // Saluti finali
+            s_stream.Close(); // Interrompi "canale di comunicazione"
+            s_client.Close(); // Saluta ed abbandona il "Cliente"
+        }
+    }
+    
+    class Program {
+        static void Main(string[] args) {
+            Server.Action();
+        }
+    }
+}
