1817ad0b873bbe38e630cbd742318bd3396992e9
Nicolò Coltro <coltro.school@gmail.com>
2021-11-29 17:28:14 +0100

    verifica socket

---
+++ Verifica_Socket/VerificaSocketTCP_Client/VerificaSocketTCP_Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace VerificaSocketTCP_Client {
+    static class Client {
+        static public void Azione() {
+            // Socket setup & Server connect
+            TcpClient c = new TcpClient("127.0.0.1", 2003);
+            NetworkStream ns = c.GetStream();
+
+            // Ask for user input
+            string n1, n2, op;
+            Console.WriteLine("Inserisci il primo numero: ");
+            n1 = Console.ReadLine();
+            Console.WriteLine("Inserisci il secondo numero: ");
+            n2 = Console.ReadLine();
+            Console.WriteLine("Inserisci l'operatore: ");
+            op = Console.ReadLine();
+
+            // Send data
+            ns.Write(System.Text.Encoding.ASCII.GetBytes(n1 + ";" + op + ";" + n2));
+
+            // Get result from Server
+            Byte[] buf = new byte[64];
+            ns.Read(buf, 0, buf.Length);
+            string payload = System.Text.Encoding.ASCII.GetString(buf);
+            Console.WriteLine(payload);
+            
+            // Close Socket things
+            ns.Close();
+            c.Close();
+        }
+    }
+    
+    class Program {
+        static void Main(string[] args) {
+            Client.Azione();
+        }
+    }
+}
---
+++ Verifica_Socket/VerificaSocketTCP_Server/VerificaSocketTCP_Server/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace VerificaSocketTCP_Server {
+    static class Server {
+        public static void Azione() {
+            // TCP Listener Setup
+            IPAddress ip = Dns.GetHostAddresses("127.0.0.1")[0];
+            TcpListener lis = new TcpListener(ip, 2003);
+            lis.Start();
+
+            // Socket setup
+            TcpClient c = lis.AcceptTcpClient();
+            NetworkStream ns = c.GetStream();
+
+            // Read from Socket
+            Byte[] buf = new byte[64];
+            ns.Read(buf, 0, buf.Length);
+            string payload = System.Text.Encoding.ASCII.GetString(buf);
+
+            // Operations
+            int n1 = int.Parse(payload.Split(';')[0]);
+            int n2 = int.Parse(payload.Split(';')[2]);
+            string op = payload.Split(';')[1];
+            int result;
+            
+            switch (op) {
+                case "+":
+                    result = n1 + n2;
+                    break;
+                case "-":
+                    result = n1 - n2;
+                    break;
+                case "*":
+                    result = n1 * n2;
+                    break;
+                case "/":
+                    result = n1 / n2;
+                    break;
+                default:
+                    result = -1;
+                    break;
+            }
+
+            // Send op
+            buf = System.Text.Encoding.ASCII.GetBytes(Environment.NewLine + n1 + " " + op + " " + n2 + " = " + result);
+            ns.Write(buf);
+
+            // Save 2 file
+            BinaryWriter bw = new BinaryWriter(File.Open(@"Z:\op.txt", FileMode.Append));
+            bw.Write(buf);
+            bw.Close();
+            
+            // Close Socket things
+            ns.Close();
+            c.Close();
+            lis.Stop();
+        }
+    }
+    
+    class Program {
+        static void Main(string[] args) {
+            Server.Azione();
+        }
+    }
+}
