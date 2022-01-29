42963a9a54ec333002e38e5a075e46c5634b170b
Nicolò Coltro <nc@elnet.work>
2022-01-29 10:18:50 +0100

    fix files

---
+++ Socket_Orologio_Multithread/O_Client/O_Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace O_Client
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente c = new Cliente();
+            c.Attività();
+        }
+    }
+
+    class Cliente
+    {
+        public void Attività()
+        {
+
+            TcpClient c;
+            NetworkStream st;
+
+            try
+            {
+
+                c = new TcpClient(Dns.GetHostName(), 10101);
+                st = c.GetStream();
+
+                while (c.Connected)
+                {
+                    byte[] bufIn = new Byte[20];
+                    String msgIn = String.Empty;
+                    Int32 bytes = st.Read(bufIn, 0, bufIn.Length);
+                    msgIn = System.Text.Encoding.ASCII.GetString(bufIn, 0, bytes);
+                    Console.WriteLine("[Client]" + " Ricevuto << " + msgIn);
+                }
+
+                st.Close();
+                c.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[Client] Errore");
+            }
+
+        }
+    }
+}
---
+++ Socket_Orologio_Multithread/O_Server/O_Server/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace O_Server
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+
+            IPAddress[] sIp = Dns.GetHostAddresses(Dns.GetHostName()); //tutti gli ip dell'host server (localhost)
+            TcpListener s = null;
+
+            try
+            {
+                s = new TcpListener(sIp[0], 10101);
+                s.Start(); //in attesa di client
+                Byte[] buf = new Byte[20];
+                String msgIn = null;
+
+                int c = 0;
+                while (true)
+                {
+                    Th Thn = new Th(s.AcceptTcpClient(), c);
+                    new Thread(Thn.Action).Start();
+                    c++;
+                }
+
+                Console.Write("[Server]" + " In attesa di connessione... \n");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[Server] Errore");
+            }
+            finally
+            {
+                s.Stop();
+            }
+
+        }
+    }
+
+    class Th
+    {
+        TcpClient dati;
+        int c;
+
+        public Th(TcpClient dati, int c)
+        {
+            this.dati = dati;
+            this.c = c;
+        }
+
+        public void Action()
+        {
+            string msgIn;
+            NetworkStream st = dati.GetStream();
+            Console.WriteLine("[Server]" + " Connesso a Th" + c);
+
+            try
+            {
+                while (dati.Connected)
+                {
+                    msgIn = DateTime.Now.ToString("dd/M/yyyy H:mm:ss");
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(msgIn);
+                    st.Write(msg, 0, msg.Length);
+                    Console.WriteLine("[Server]" + " Spedito >> " + msgIn + " a Th" + c);
+                    Thread.Sleep(1000);
+                }
+
+                st.Close();
+                dati.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[Server] Il client " + c + " si è disconnesso");
+            }
+        }
+    }
+}
