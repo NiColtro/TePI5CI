b1c54097ff6a5495d50304f053c3bf3bf452d568
Nicolò Coltro <nc@elnet.work>
2022-02-04 21:12:24 +0100

    chat

---
+++ Socket_Chat_Multithread/Client/Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Chat_Client
+{
+    internal class MessageHandler
+    {
+        TcpClient _client;
+        public NetworkStream stream;
+        bool _ready; // Ready to read from stream
+
+        public MessageHandler(TcpClient client)
+        {
+            _client = client;
+            _ready = false;
+            stream = _client.GetStream(); // Get stream from client
+
+            new Thread(MsgRecive).Start(); // Start thread
+        }
+
+        public void MsgRecive()
+        {
+            while (true) // While connected
+            {
+                if (_ready) // If ready to use stream
+                {
+                    byte[] buf = new byte[1024];
+                    int len = stream.Read(buf, 0, buf.Length); // Read from stream
+
+                    string payload = Encoding.ASCII.GetString(buf, 0, len);
+                    Console.WriteLine("\n" + payload); // Write on screen
+                }                
+            }
+        }
+
+        public void MsgSend(string payload)
+        {
+            stream.Write(Encoding.ASCII.GetBytes(payload)); // Send
+            _ready = true;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient c = new TcpClient(); // Setup
+            c.Connect("127.0.0.1", 2022);
+
+            MessageHandler mh = new MessageHandler(c); // Init new message handler
+            
+            Console.Write("[?] Choose your username: ");
+            new Thread(mh.MsgRecive).Start(); // Start th: listen to incoming messages
+
+            while (true)
+            {
+                string payload = Console.ReadLine();
+                mh.MsgSend(payload);
+            }
+        }
+    }
+}
---
+++ Socket_Chat_Multithread/Server/Server/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Chat_Server
+{
+    static class Storage
+    {
+        public static List<ClientHandler> clients; // List of connected users
+    }
+
+    internal class ClientHandler
+    {
+        TcpClient _client;
+        NetworkStream _stream;
+        string? _username;
+
+        public ClientHandler(TcpClient client)
+        {
+            _client = client;
+            _stream = _client.GetStream(); // Get stream from client
+
+            new Thread(Action).Start(); // Start thread
+        }
+
+        public void Action()
+        {
+            Console.WriteLine("[!] Handling new client...");
+
+            try
+            {
+                while (_client.Connected)
+                {
+                    byte[] buf = new byte[1024];
+                    int len = _stream.Read(buf, 0, buf.Length); // Read from stream
+
+                    string payload = Encoding.ASCII.GetString(buf, 0, len);
+
+                    if (_username == null)
+                    {
+                        _username = payload;
+                        Broadcast("[+] " + _username + " joined the chat.");
+                    }
+                    else
+                        Broadcast("[M] " + _username + " => " + payload);
+                }
+            }
+            catch (Exception ex)
+            {
+                Storage.clients.Remove(this);
+                Broadcast("[-] " + _username + " left the chat.");
+            }
+            finally
+            {
+                _stream.Close();
+                _client.Close();
+            }
+        }
+
+        public void SendMessage(string s) // Send message to user object
+        {
+            Console.WriteLine("[!] Sending '" + s + "' to " + _username);
+            _stream.Write(Encoding.ASCII.GetBytes(s));
+        }
+
+        public void Broadcast(string s) // Iterate and send message to every user
+        {
+            Storage.clients.ForEach(c =>
+            {
+                if (c != this && c._username != null) // If target is not the sender itself
+                    c.SendMessage(s);
+            });
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Storage.clients = new List<ClientHandler>();
+
+            TcpListener lis = new TcpListener(System.Net.IPAddress.Loopback, 2022); // Setup
+            lis.Start();
+
+            while (true)
+            {
+                Storage.clients.Add(new ClientHandler(lis.AcceptTcpClient()));
+            }
+        }
+    }
+}
